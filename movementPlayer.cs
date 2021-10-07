f4e929809513962551ba509eb1d687518d593c96
Felix <59079877+FelixVonPluschzyk@users.noreply.github.com>
2021-10-07 20:26:53 +0200

    First Upload
    The Manager :
    
    The manager is responsible for the game processes.
    The following examples fall under his tasks:
    Variables management.
    Ui management.
    Scenes management.
    Sound management.
    (possible backup management so that after mini-games the scene is the same as before the start).
    
    
    Movement:
    
    
    the movement script is meant for the mevement of the player, but i would also manage player animation in this script 
    
    
    
    NPC Skript:
    
    
    I thought that we could give the NPC tags to identify them in this script, then we could interact with them individually without creating too many scripts.

---
+++ movementPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementPlayer : MonoBehaviour
+{
+    Rigidbody2D body;
+    public Animator anim;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 1.0f;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.W)) {
+            anim.SetBool("up", true);
+            anim.SetBool("down", false);
+            anim.SetBool("right", false);
+            anim.SetBool("left", false);
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+            anim.SetBool("right", false);
+            anim.SetBool("left", true);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+            anim.SetBool("right", true);
+            anim.SetBool("left", false);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            anim.SetBool("up", false);
+            anim.SetBool("down", true);
+            anim.SetBool("right", false);
+            anim.SetBool("left", false);
+        }
+    }
+
+    private void FixedUpdate() {
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
