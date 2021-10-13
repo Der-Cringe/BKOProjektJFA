e0dab05ad00a59fe3a4386ffac74c8815b8f9843
FelixVonPluschzyk <felix.pluschzyk@gmail.com>
2021-10-13 14:28:43 +0200

    add Psst Button

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Manager : MonoBehaviour
-{
-    
-    
+public class Manager:MonoBehaviour {
+
+
     //  Private Section
     //  Value that Contains the curretn Dezibel Value
     //  We use this to Controll the dezibel Slider
     private int VolumeValue;
     public Slider DezibelSlider;
+    
 
 
     private void Start() {
         VolumeValue = 20;
     }
     private void Update() {
+        if(VolumeValue > DezibelSlider.maxValue) {
+            VolumeValue = (int)DezibelSlider.maxValue;
+        }else if(VolumeValue < 0) {
+            VolumeValue = 0;
+        }
         setSlider();
     }
 
@@ -29,6 +34,12 @@ public class Manager : MonoBehaviour
     // Attribut Controll Methods
     public void setVolumeValue(int newVal) {
         VolumeValue = newVal;
+    }
+    public void AddVolumeValue(int newVal) {
+        VolumeValue += newVal;
+    }
+    public void removeVolumeValue(int newVal) {
+        VolumeValue -= newVal;
     }
     public int getVolumeValue() {
         return VolumeValue;
