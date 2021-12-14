d29e8df2e1f3d659c471c10e7bbbff23ddd7507c
FelixVonPluschzyk <felix.pluschzyk@gmail.com>
2021-12-14 21:56:46 +0100

    door thing von josef

---
+++ Assets/Skripts/StartstdSettingMg.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StartstdSettingMg : MonoBehaviour
+{
+     [SerializeField] Texture2D cursor;
+    void Start() {
+        Cursor.SetCursor(cursor,Vector3.zero,CursorMode.ForceSoftware);
+    }
+}
