21763cf6e7f007f18fcc8ea6c73f04b11f75c451
FelixVonPluschzyk <felix.pluschzyk@gmail.com>
2021-12-16 18:43:08 +0100

    DragnDrop

--- Assets/pcPluger.cs
+++ Assets/pcPluger.cs
@@ -6,34 +6,50 @@ using UnityEngine.UI;
 
 public class pcPluger : MonoBehaviour{
 
-    public Image img;
+    public GameObject Target;
+    public GameObject empty;
+
+    public GameObject rechts;
+    public GameObject links;
+    public GameObject[] spawnpoint;
+    private int id;
+
+    public GameObject Kabel;
+    public GameObject Kabel_transparent;
     private bool isDragging;
 
-    public LineRenderer line;
-    public DistanceJoint2D joint;
-    DistanceJoint2D rope;
-
-    bool checker;
-
     void Start() {
-        gameObject.AddComponent<LineRenderer>();
+        Target = empty;
     }
 
 
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
             if(isDragging == true) {
+                Kabel.SetActive(true);
+                Kabel_transparent.SetActive(false);
                 isDragging = false;
             } else if(isDragging == false) {
+                Kabel.SetActive(false);
+                Kabel_transparent.SetActive(true);
                 isDragging = true;
+                Target = empty;
+
             }
 
         }
         if(isDragging) {
             Vector3 mousePos = Input.mousePosition;
-            img.transform.position = mousePos;
+            Target.transform.position = mousePos;
             
         }
     }
-
+    public void change_target_to_left() {
+        Target = links;
+        id = 0;
+    }
+    public void change_target_to_right() {
+        Target = rechts;
+        id = 1;
+    }
 }
