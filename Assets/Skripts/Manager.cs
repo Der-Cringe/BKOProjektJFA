d4157190ac9b05b38240cc6f165744453f577d77
FelixVonPluschzyk <felix.pluschzyk@gmail.com>
2021-12-16 15:13:45 +0100

    comments

--- Assets/Skripts/Manager.cs
+++ Assets/Skripts/Manager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
+
 public class Manager:MonoBehaviour {
 
 
@@ -13,40 +14,76 @@ public class Manager:MonoBehaviour {
     //  We use this to Controll the dezibel Slider
     private const float REAL_SECONDS_PER_INGAME_DAY = 600f;
 
+
+
+    ///<Variablen>
+    ///     Player Objekt
+    /// </Variablen>
+    public GameObject Player;
+
+    /// <Variablen>
+    ///     Room Setups
+    /// </Variablen>
     public Transform SpawnPointFloorPos;
     public Transform SpawnPointClassPos;
-    public GameObject Player;
     public GameObject StudentsClass;
     public GameObject StudentsFloor;
 
 
+    /// <Variablen>
+    ///     UI Abteile
+    /// </Variablen>
     public GameObject AgainScreen;
+    public GameObject fullInGameUi;
+
+    /// <Variablen>
+    ///     Ui Single Elemente
+    /// </Variablen>
     private int VolumeValue;
     private int knowledgeValue;
-    private float day = 0.3128f;
-
     public Slider DezibelSlider;
-
     public Slider knowledgeSlider;
     public Text clockText;
+    private int score;
 
+    /// <Variablen>
+    ///     Time
+    /// </Variablen>
+    private float day = 0.3128f;
+
+    /// <Variablen>
+    ///     Interakt Variablen 
+    /// </Variablen>
     private bool interaktiv;
     public GameObject interaktivObj;
-    public GameObject fullInGameUi;
     public int InteraktId;
 
-
+    /// <Variablen>
+    ///     Minigame Verwaltung
+    /// </Variablen>
     public GameObject[] mgs;
     public int currentMg;
 
-
+    /// <Variablen>
+    ///     NPC Variablen
+    /// </Variablen>
     public GameObject[] talkingBubbles;
     public GameObject[] npcs;
     private double nextDezibelUp;
-    private int randomNpcDezCount;
+    private int randomNpcDezCount;    ///     Muss ist zukunft nicht Random sein
 
-    private int score;
 
+
+    /// <summary>
+    ///     Diese von unity vorgegebnen FUnktionen kümmern sich um den start und 
+    ///     den durchlauf des games hier wird sich um folgendes gekürmmert
+    ///     Start
+    ///         reseten Wichtiger Variablen
+    ///         setzten von Voreinstellung
+    ///     Update
+    ///         Setzten von Wichtigen Ui Elementen die dauer Aktuell sein müssen
+    ///         
+    /// </summary>
     private void Start() {
         knowledgeValue = 0;
         setknowledgeSlider();
@@ -82,26 +119,56 @@ public class Manager:MonoBehaviour {
         
     }
 
-
+    /// <summary>
+    ///     Die folgenden Funktionen kümmern sich um das vorsetup/setup/übergänge welchen auch in Start benutzt wird
+    ///         SpawnPlayer : Spawnt Player im Flur
+    ///         startDoor   : Leitet player weiter zum Raum 
+    ///         
+    /// </summary>
     private void SpawnPlayer()
     {
         Player.transform.position = SpawnPointFloorPos.position;
     }
-
-
-
-
-
-
-    void setDezibelSlider() {
-        DezibelSlider.value = VolumeValue;
+    public void startDoor() {
+        Player.transform.position = SpawnPointClassPos.position;
+        StudentsClass.SetActive(true);
+        StudentsFloor.SetActive(false);
     }
 
-    public void setknowledgeSlider(){
-        knowledgeSlider.value = knowledgeValue;
+
+    /// <summary>
+    /// Die Folgenden Funktionen sind die Minigame verwaltungen 
+    ///     doorMinigame    :   zeigt das doorMinigame an
+    ///         
+    /// </summary>
+    /// 
+    private void doorMinigame() {
+        fullInGameUi.SetActive(false);
+        mgs[1].SetActive(true);
+    }
+    public void startQuest() {
+        doorMinigame();
+    }
+    private void minigame() {
+        fullInGameUi.SetActive(false);
+        //random pic
+        currentMg = Random.Range(0,mgs.Length - 1);
+        mgs[currentMg].GetComponent<stdSettingMgSkript>().setHardnessLvl(VolumeValue);
+        mgs[currentMg].SetActive(true);
+    }
+    public void sendResultsToMgmt(int uscore) {
+        mgs[currentMg].SetActive(false);
+        fullInGameUi.SetActive(true);
+        // Setztung des Knowledge Wert aufbasis der Resultate des minigames
+        knowledgeValue = knowledgeValue + uscore / 10;
+
+
     }
 
-    // Attribut Controll Methods
+
+    /// <summary>
+    ///     Volume Manipulation
+    /// </summary>
     public void setVolumeValue(int newVal) {
         VolumeValue = newVal;
     }
@@ -114,36 +181,13 @@ public class Manager:MonoBehaviour {
     public int getVolumeValue() {
         return VolumeValue;
     }
-
-    public void setTime(){
-        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
-
-        float dayNormalized = day % 1f;
-        float hoursPerDay = 24f;
-        
-        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
-
-        float minutesPerHour = 60f;
-        string minutesString = Mathf.Floor(((dayNormalized* hoursPerDay) % 1f) * minutesPerHour).ToString("00");
-        
-        clockText.text = hoursString + ":" +minutesString;
-
-        // Wenn die Zeit 14:30 beträgt wird eine methode aufgerufen da dies das Ende des Unterrichts ist!
-        if(day >= 0.6027f){
-            gameTimeEnd();
-        }
+    void setDezibelSlider() {
+        DezibelSlider.value = VolumeValue;
     }
 
-        private void doorMinigame() {
-        fullInGameUi.SetActive(false);
-        mgs[1].SetActive(true);
-    }
-
-    public void gameTimeEnd(){
-        day = 0.3128f;
-        score = knowledgeValue;
-        AgainScreen.SetActive(true);
-    }
+    /// <summary>
+    ///    Interaktiv Verwaltung 
+    /// </summary>
     public void interaktivSet(bool a,GameObject b,int id) {
         interaktiv = a;
         interaktivObj = b;
@@ -152,26 +196,41 @@ public class Manager:MonoBehaviour {
     public bool GetInteraktiv() {
         return interaktiv;
     }
-    public void startQuest() {
-        doorMinigame();
-        
+
+    /// <summary>
+    ///     Zeit Funktionen
+    ///         setTime : Schulzeitverwaltung
+    ///         gameTimeEnd : wenn die Zeit um ist 
+    /// </summary>
+    public void setTime() {
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+
+        float dayNormalized = day % 1f;
+        float hoursPerDay = 24f;
+
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+
+        float minutesPerHour = 60f;
+        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
+
+        clockText.text = hoursString + ":" + minutesString;
+
+        // Wenn die Zeit 14:30 beträgt wird eine methode aufgerufen da dies das Ende des Unterrichts ist!
+        if(day >= 0.6027f) {
+            gameTimeEnd();
+        }
     }
-    // Mini Games
-    private void minigame() {
-        fullInGameUi.SetActive(false);
-        //random pic
-        currentMg = Random.Range(0,mgs.Length-1);
-        mgs[currentMg].GetComponent<stdSettingMgSkript>().setHardnessLvl(VolumeValue);
-        mgs[currentMg].SetActive(true);
+    public void gameTimeEnd() {
+        day = 0.3128f;
+        score = knowledgeValue;
+        AgainScreen.SetActive(true);
     }
-    public void sendResultsToMgmt(int uscore) {
-        mgs[currentMg].SetActive(false);
-        fullInGameUi.SetActive(true);
-        // Setztung des Knowledge Wert aufbasis der Resultate des minigames
-        knowledgeValue = knowledgeValue + uscore /10;
 
 
-    }
+    /// <summary>
+    ///     NPC Funktionen
+    ///     
+    /// </summary>
     private void makeNoises(int count) {
         for(int i = 0 ; i < count;i++ ) {
             NPCskript tmpSkript = npcs[Random.Range(0,npcs.Length - 1)].GetComponent<NPCskript>();
@@ -181,6 +240,14 @@ public class Manager:MonoBehaviour {
                 tmpSkript.talk(talkingBubbles[Random.Range(0,talkingBubbles.Length-1)]);
             }
         }
+    }
+  
+    
+    /// <summary>
+    ///     UI Funktionen
+    /// </summary>
+    public void setknowledgeSlider(){
+        knowledgeSlider.value = knowledgeValue;
     }
     public int get_score()
     {
@@ -195,20 +262,12 @@ public class Manager:MonoBehaviour {
         score = 0;
     }
 
+    /// <summary>
+    /// Scenen Management Funktionen
+    /// </summary>
     public void restart_game()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    public void startDoor()
-    {
-        Debug.Log("fuck");
-        //door game
-        changeToMainRoom();
-    }
-    public void changeToMainRoom()
-    {
-        Player.transform.position = SpawnPointClassPos.position;
-        StudentsClass.SetActive(true);
-        StudentsFloor.SetActive(false);
-    }
+
 }
